e064fd12bad3c1ec830da64c32c08f699ed203fd
Sarath <saratonite@gmx.com>
2018-05-16 10:52:33 +0530

    :tomato: popmotion spring example

--- src/popmotion/app.js
+++ src/popmotion/app.js
@@ -1,6 +1,8 @@
 import './style.css';
 import { easing, tween, styler } from 'popmotion';
 
+import { spring, listen, value, pointer } from 'popmotion';// Example 2
+
 // Example 1
 const divStyler = styler(document.querySelector('.box1'));
 
@@ -14,3 +16,27 @@ tween({
   // loop: 5,
   // yoyo: 5
 }).start(divStyler.set);
+
+
+// Example 2 
+const ball = document.querySelector('.ball');
+const divBallStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divBallStyler.set);
+
+listen(ball,'mousedown touchstart')
+    .start(e=> {
+      e.preventDefault();
+      pointer(ballXY.get()).start(ballXY); // Pointer means the mouse pointer
+
+    })
+
+listen(document, 'mouseup touchend')
+      .start(() => {
+        spring({
+         from: ballXY.get(),
+         velocity: ballXY.getVelocity(),
+         to: {x:0, y:0},
+         stiffness: 200
+        }).start(ballXY);
+      })
+
